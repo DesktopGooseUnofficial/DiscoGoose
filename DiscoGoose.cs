7032f7e4c6f31a669803c2eb08cd50cd81b6e55a
VeeDeeOh <veenus224.7@gmail.com>
2020-02-10 03:24:33 -0600

    Initial commit

---
+++ DiscoGoose.cs
@@ -0,0 +1,39 @@
+﻿using GooseShared;
+using SamEngine;
+using System.Drawing;
+
+namespace DiscoGoose
+{
+    public class DiscoGoose : IMod
+    {
+        public RainbowRender whiteRender;
+        public RainbowRender orangeRender;
+        
+        private bool started = false;
+
+        // Gets called automatically, passes in a class that contains pointers to
+        // useful functions we need to interface with the goose.
+        void IMod.Init()
+        {
+            // Subscribe to whatever events we want
+            InjectionPoints.PreRenderEvent += new InjectionPoints.PreRenderEventHandler(PreRenderEvent);
+        }
+
+        public void PreRenderEvent(GooseEntity goose, Graphics gfx)
+        {
+            if(!started)
+            {
+                whiteRender = new RainbowRender(goose.renderData.brushGooseWhite.Color);
+                orangeRender = new RainbowRender(goose.renderData.brushGooseOrange.Color);
+                
+                started = true;
+            }
+
+            whiteRender.UpdateColor();
+            orangeRender.UpdateColor();
+
+            goose.renderData.brushGooseWhite.Color = whiteRender.AsColor();
+            goose.renderData.brushGooseOrange.Color = orangeRender.AsColor();
+        }
+    }
+}
---
+++ RainbowRender.cs
@@ -0,0 +1,87 @@
+﻿using System.Drawing;
+
+namespace DiscoGoose
+{
+    public class RainbowRender
+    {
+        public int r;
+        public int g;
+        public int b;
+        public int state;
+
+        public Color AsColor()
+        {
+            return Color.FromArgb(r, g, b);
+        }
+
+        public RainbowRender(Color color)
+        {
+            r = color.R;
+            g = color.G;
+            b = color.B;
+        }
+
+        /* Loops don't really work here,
+         * so a dirty solution it is.
+             
+         * TODO: See if there's a way I can clean this up
+        */
+        public void UpdateColor()
+        {
+            if (state == 0)
+            {
+                g++;
+                if (g >= 255)
+                {
+                    g = 255;
+                    state = 1;
+                }
+            }
+            if (state == 1)
+            {
+                r--;
+                if (r <= 0)
+                {
+                    r = 0;
+                    state = 2;
+                }
+            }
+            if (state == 2)
+            {
+                b++;
+                if (b >= 255)
+                {
+                    b = 255;
+                    state = 3;
+                }
+            }
+            if (state == 3)
+            {
+                g--;
+                if (g <= 0)
+                {
+                    g = 0;
+                    state = 4;
+                }
+            }
+            if (state == 4)
+            {
+                r++;
+                if (r >= 255)
+                {
+                    r = 255;
+                    state = 5;
+                }
+            }
+            if (state == 5)
+            {
+                b--;
+                if (b <= 0)
+                {
+                    b = 0;
+                    state = 0;
+                }
+            }
+        }
+    }
+}
